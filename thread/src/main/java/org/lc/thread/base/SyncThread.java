4df1a177580602378b7f915779245ffb89af94e2
chaoliu1025 <chaoliu1025@163.com>
2018-05-18 09:29:47 +0800

    测试 synchronized (String.class) {} 加锁

--- thread/src/main/java/org/lc/thread/base/SyncThread.java
+++ thread/src/main/java/org/lc/thread/base/SyncThread.java
@@ -1,23 +1,13 @@
 package org.lc.thread.base;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.BasicConfigurator;
 
 public class SyncThread implements Runnable{
 	
-    /**
-     * 日志
-     */
-    private static final Log LOGGER = LogFactory.getLog(SyncThread.class);
 
-    private static  Integer value;
+    private Integer value;
 
     private static Integer NOWVALUE;
 
-    static {
-        BasicConfigurator.configure();
-    }
 
     public SyncThread(int value) {
         this.value = value;
@@ -26,17 +16,17 @@ public class SyncThread implements Runnable{
     /**
      * 对这个类的实例化对象进行检查
      */
-    private synchronized void doOtherthing() {
-        NOWVALUE = this.value;
-        LOGGER.info("当前NOWVALUE的值：" + NOWVALUE);
-    }
-    
-//    private  void doOtherthing() {
-//    	synchronized (this) {
-//    		NOWVALUE = value;
-//    		LOGGER.info("当前NOWVALUE的值：" + NOWVALUE);
-//		}
+//    private synchronized void doOtherthing() {
+//        NOWVALUE = this.value;
+//        LOGGER.info("当前NOWVALUE的值：" + NOWVALUE);
 //    }
+    
+    private  void doOtherthing() {
+    	synchronized (String.class) {
+    		NOWVALUE = value;
+    		System.out.println("当前NOWVALUE的值：" + NOWVALUE);
+		}
+    }
     
 //    private static synchronized void doOtherthing() {
 //    	NOWVALUE = value;
@@ -59,20 +49,74 @@ public class SyncThread implements Runnable{
         Long id = currentThread.getId();
         this.doOtherthing();
     }
+    
+    
 
     public static void main(String[] args) throws Exception {
-//        Thread syncThread1 = new Thread(new SyncThread(10));
-//        Thread syncThread2 = new Thread(new SyncThread(100));
-//
-//        syncThread1.start();
-//        syncThread2.start();
-    	SyncThread syncThread = new SyncThread(10);
-        Thread syncThread1 = new Thread(syncThread);
-        Thread syncThread2 = new Thread(syncThread);
-        
+        Thread syncThread1 = new Thread(new SyncThread(10));
+        Thread syncThread2 = new Thread(new SyncThread1(100));
+
         syncThread1.start();
         syncThread2.start();
+//    	SyncThread syncThread = new SyncThread(10);
+//        Thread syncThread1 = new Thread(syncThread);
+//        Thread syncThread2 = new Thread(syncThread);
+//        
+//        syncThread1.start();
+//        syncThread2.start();
     }
+    
+    
 
 
 }
+
+ class SyncThread1 implements Runnable{
+	
+
+    private static  Integer value;
+
+    private static Integer NOWVALUE;
+
+
+    public SyncThread1(int value) {
+        this.value = value;
+    }
+
+    /**
+     * 对这个类的实例化对象进行检查
+     */
+//    private synchronized void doOtherthing1() {
+//        NOWVALUE = this.value;
+//        LOGGER.info("当前NOWVALUE的值：" + NOWVALUE);
+//    }
+    
+    private  void doOtherthing1() {
+    	synchronized (String.class) {
+    		NOWVALUE = value;
+    		System.out.println("当前NOWVALUE的值：" + NOWVALUE);
+		}
+    }
+    
+//    private static synchronized void doOtherthing() {
+//    	NOWVALUE = value;
+//    	LOGGER.info("当前NOWVALUE的值：" + NOWVALUE);
+//    }
+    
+    
+    
+//    private void doOtherthing() {
+//    	synchronized (SyncThread.class) {
+//    		NOWVALUE = this.value;
+//    		LOGGER.info("当前NOWVALUE的值：" + NOWVALUE);
+//		}
+//    }
+    
+
+    @Override
+    public void run() {
+        Thread currentThread = Thread.currentThread();
+        Long id = currentThread.getId();
+        this.doOtherthing1();
+    }
+  }
